6c252e1e9c6f72f88ceda424c760449a987213fd
Ramon Muñoz <ramonm2811@gmail.com>
2021-10-31 11:07:11 -0300

    aja

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ if (mark.bmi > john.bmi) {
 );
  */
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+/* const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const total = [];
 
@@ -134,3 +134,55 @@ function calcAverage(arr) {
 calcAverage(bills);
 calcAverage(tips);
 calcAverage(total);
+ */
+
+var person = {
+  name: "John", //String
+  lastname: "Doe",
+  age: 35, //Number
+  gender: "male",
+  lucky_numbers: [7, 11, 13, 17], //Array
+  significant_other: person2, //Object, yes the same variable/object defined after
+};
+
+var person2 = {
+  name: "Jane",
+  lastname: "Doe",
+  age: 38,
+  gender: "female",
+  lucky_numbers: [2, 4, 6, 8],
+  significant_other: person,
+};
+
+var person3 = {};
+
+var family = {
+  lastname: "Doe",
+  members: [person, person2], //Array of objects, don't forget to add Jimmy
+};
+
+function addAllFamilyLuckyNumbers(anArray) {
+  var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
+
+  for (let i = 0; i < anArray.length; i++) {
+    for (let j = 0; j < anArray[i].lucky_numbers.length; j++) {
+      sumOfAllLuckyNumbers += anArray[i].lucky_numbers[j];
+    }
+  }
+  return sumOfAllLuckyNumbers;
+}
+
+//Enter all your code here:
+family.members[0].lucky_numbers[3] = 33;
+
+family.members.push(person3);
+
+family.members[2].name = "Jimmy";
+family.members[2].lastname = "Doe";
+family.members[2].age = 13;
+family.members[2].gender = "Male";
+family.members[2].lucky_numbers = [1, 2, 3, 4];
+family.members[2].significant_other = null;
+
+//Do not make changes below:
+console.log(addAllFamilyLuckyNumbers(family.members));
